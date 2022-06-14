054717eee7c9d3d902e15cfcea70c4a2df31e553
LucasMSF <lucasgg003@gmail.com>
2022-06-14 17:36:34 -0300

    Adição de máscara no input

--- src/components/CepForm.tsx
+++ src/components/CepForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import InputMask from 'react-input-mask';
 
 type Props = {
     getCep(cep: string): any
@@ -15,7 +16,7 @@ const CepForm = (props: Props) => {
     return (
         <form onSubmit={handleSubmit}>
             <div className="form-group mt-5">
-                <input id='txtCep' onChange={({target: {value}}) => {setCep(value)}} className='outline-sky-600 p-2 rounded-md bg-gray-300' type="text" placeholder='CEP' />
+                <InputMask id='txtCep' onChange={({target: {value}}) => {setCep(value)}} className='outline-sky-600 p-2 rounded-md bg-gray-300' type="text" placeholder='CEP'  mask="99999-999"/>
                 <button className='bg-sky-600 p-2 text-white ml-2 rounded-md hover:bg-sky-500 active:bg-sky-500 focus:outline-none focus:ring focus:ring-sky-600 font-bold' type="submit">Verificar</button>
             </div>
     </form >
